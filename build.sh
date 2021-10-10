89c9737f46189f66884c9007628d0568312d74b0
myl7 <myl@myl.moe>
2021-10-11 03:12:25 +0800

    build: Fix script and add CI

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 function build {
   (cd twitter-text-parse-go && cargo build --release --target $1)
-  cp twitter-text-parse-go/target/release/libtwitter_text_parse_go.a lib/libtwitter_text_parse_go-$1.a
+  cp twitter-text-parse-go/target/$1/release/libtwitter_text_parse_go.a lib/libtwitter_text_parse_go-$1.a
 }
 
 mkdir -p lib
