dd932f1604be18bb50b0d39af876ffa9089caea2
myl7 <myl@myl.moe>
2021-10-14 06:16:37 +0800

    build: Rename dockerfile used for musl

--- build/build.sh
+++ build/build.sh
@@ -8,6 +8,6 @@ target=x86_64-unknown-linux-gnu
 cp twitter-text-parse-go/target/$target/release/libtwitter_text_parse_go.a lib/libtwitter_text_parse_go-$target.a
 
 target=x86_64-unknown-linux-musl
-docker build -t build -f build/musl/Dockerfile .
+docker build -t build -f build/musl.dockerfile .
 docker run --name build build
 docker cp build:/app/libtwitter_text_parse_go.a lib/libtwitter_text_parse_go-$target.a
